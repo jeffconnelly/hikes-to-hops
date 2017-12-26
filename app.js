c368f5cd08a2bea9ecf1d74e1ec9e8a8ef407248
Jeff Connelly <jeff.connelly6@gmail.com>
2017-12-26 14:01:32 -0700

    Added server and call to trails API

--- app.js
+++ app.js
@@ -5,14 +5,7 @@ const clientKey = 'nxJ4T31JaYmshZujaPeoLhL0g2lop1H7pi9jsn51LSvRMryZte';
 
 
 
-unirest.get("https://trailapi-trailapi.p.mashape.com/?lat=34.1&limit=25&lon=-105.2&q[activities_activity_type_name_eq]=hiking&q[city_cont]=Denver&q[country_cont]=Australia&q[state_cont]=California&radius=25")
-.header("X-Mashape-Key", "jvHLN2ffP4mshDADMnyUd0OkSXaXp16hQKqjsnIpARwMYTtPWp")
-.header("Accept", "text/plain")
-.end(function (result) {
-  console.log(result.status, result.headers, result.body);
-});
 
-$unirest();
 
 
 
---
+++ server.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const express = require('express');
+const unirest = require('unirest');
+const morgan = require('morgan');
+const app = express();
+
+
+app.use(morgan('common'));
+
+// These code snippets use an open-source library. http://unirest.io/nodejs
+unirest.get('https://trailapi-trailapi.p.mashape.com/?limit=1&lon=-&q[activities_activity_type_name_eq]=hiking&q[city_cont]=Colorado+Springs&q[country_cont]=United+States&q[state_cont]=Colorado&radius=10')
+  .header('X-Mashape-Key', 'jvHLN2ffP4mshDADMnyUd0OkSXaXp16hQKqjsnIpARwMYTtPWp')
+  .header('Accept', 'text/plain')
+  .end(function (result) {
+    console.log(result.body);
+  });
+
+
+  
+let server;
+
+function runServer() {
+  const port = process.env.PORT || 8080;
+  return new Promise((resolve, reject) => {
+    server = app.listen(port, () => {
+      console.log(`Your app is listening on port ${port}`);
+      resolve(server);
+    }).on('error', err => {
+      reject(err);
+    });
+  });
+}
+
+function closeServer() {
+  return new Promise((resolve, reject) => {
+    console.log('Closing server');
+    server.close(err => {
+      if (err) {
+        reject(err);
+        // so we don't also call `resolve()`
+        return;
+      }
+      resolve();
+    });
+  });
+}
+
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+} 
+
+module.exports = {app, runServer, closeServer};
