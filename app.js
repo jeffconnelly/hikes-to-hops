cd8c2e75eae6126b103ef8900b39e5574e24cbfe
Jeff Connelly <jeff.connelly6@gmail.com>
2017-12-26 13:24:32 -0700

    Created server file

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 'use strict';
 
-console.log('hello');
+
+const clientKey = 'nxJ4T31JaYmshZujaPeoLhL0g2lop1H7pi9jsn51LSvRMryZte';
 
 
 
-function watchSubmit() {
-  console.log('sup');
-}
+unirest.get("https://trailapi-trailapi.p.mashape.com/?lat=34.1&limit=25&lon=-105.2&q[activities_activity_type_name_eq]=hiking&q[city_cont]=Denver&q[country_cont]=Australia&q[state_cont]=California&radius=25")
+.header("X-Mashape-Key", "jvHLN2ffP4mshDADMnyUd0OkSXaXp16hQKqjsnIpARwMYTtPWp")
+.header("Accept", "text/plain")
+.end(function (result) {
+  console.log(result.status, result.headers, result.body);
+});
 
-$(watchSubmit);
+$unirest();
+
+
+
