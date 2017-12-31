5de04fd61d191dfc18325f23a7bdb9a787573a0d
Jeff Connelly <jeff.connelly6@gmail.com>
2017-12-31 14:42:19 -0700

    Cleaned up server.js - did not need unirest for hiking API anymore.

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const express = require('express');
-const unirest = require('unirest');
 const morgan = require('morgan');
 const cors = require('cors');
 const bodyParser = require('body-parser');
@@ -12,35 +11,16 @@ app.use(express.static('public'));
 app.use(bodyParser.json()); // parse JSON body
 app.use(cors());
 
-// These code snippets use an open-source library. http://unirest.io/nodejs
 
 let result;
 
-
-// console.log(Request.body);
-
 app.get('/', (req, res) => {
-  // const item = Request.body;
   console.log('request made!');
   console.log(bodyResult);
-  // console.log(item);
   res.json(bodyResult);
 });
 
 let bodyResult;
-
-let Request;
-//Capture API Data from TrailAPI
-Request = unirest.get('https://trailapi-trailapi.p.mashape.com/?limit=1&lon=-&q[activities_activity_type_name_eq]=hiking&q[city_cont]=Colorado+Springs&q[country_cont]=United+States&q[state_cont]=Colorado&radius=10')
-  .header('X-Mashape-Key', 'jvHLN2ffP4mshDADMnyUd0OkSXaXp16hQKqjsnIpARwMYTtPWp')
-  .header('Accept', 'text/plain')
-  .end(function (result) {
-    // console.log(result.body);
-    bodyResult = result.body;
-    // console.log(bodyResult);
-    return bodyResult;
-  });
-
 
 
 //Server functions
